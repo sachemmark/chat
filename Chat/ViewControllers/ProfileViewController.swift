8c0500bb70ce20a536d8813f90dcbb98b911e294
Mark <sachemmark@gmail.com>
2018-10-22 16:09:29 +0300

    Temporary

--- Chat/ViewControllers/ProfileViewController.swift
+++ Chat/ViewControllers/ProfileViewController.swift
@@ -13,8 +13,13 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var profileImageVIew: UIImageView!
     @IBOutlet weak var photoButton: UIButton!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
 
+    @IBOutlet weak var operationButton: UIButton!
+    @IBOutlet weak var GCDButton: UIButton!
+    
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -44,6 +49,16 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
         }
     }
     
+    
+    @objc func startEditing() {
+        editButton.isHidden = true
+        GCDButton.isHidden = false
+        operationButton.isHidden = false
+        descriptionTextField.isEnabled = true
+    }
+    
+    
+    
     @objc func photoButtonAction() {
         let alert = UIAlertController(title: "Set profile photo", message: "", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("Choose from Library", comment: "Default action"), style: .default, handler: { _ in
@@ -55,29 +70,52 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
         self.present(alert, animated: true, completion: nil)
     }
     
+    func getChangedData() ->
+        
+    @objc func gcdSave() {
+        
+    }
+    
+    @objc func operationSave() {
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        editButton.layer.cornerRadius = 15
+        descriptionTextField.backgroundColor = UIColor.clear
+        descriptionTextField.borderStyle = .none
+        
+        editButton.layer.cornerRadius = 16
         editButton.layer.borderWidth = 2
         editButton.layer.borderColor = UIColor.black.cgColor
+        editButton.addTarget(self, action: #selector(ProfileViewController.startEditing), for: .touchUpInside)
         
         profileImageVIew.layer.cornerRadius = 30
         profileImageVIew.layer.masksToBounds = true
         
         photoButton.layer.cornerRadius = 30
         photoButton.addTarget(self, action: #selector(ProfileViewController.photoButtonAction), for: .touchUpInside)
-        
-        print(editButton.frame) // SE and X have differnet screen sizes, so it's not yet recalculated here
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        descriptionLabel.sizeToFit()
-        print(editButton.frame)
+        GCDButton.frame.size.width = (view.frame.width - 48) / 2
+        GCDButton.layer.cornerRadius = 16
+        GCDButton.layer.borderWidth = 2
+        GCDButton.layer.borderColor = UIColor.black.cgColor
+        GCDButton.addTarget(self, action: #selector(ProfileViewController.gcdSave), for: .touchUpInside)
+        
+        operationButton.frame.size.width = (view.frame.width - 48) / 2
+        operationButton.frame.origin.x = GCDButton.frame.maxX + 16
+        operationButton.layer.cornerRadius = 16
+        operationButton.layer.borderWidth = 2
+        operationButton.layer.borderColor = UIColor.black.cgColor
+        operationButton.addTarget(self, action: #selector(ProfileViewController.operationSave), for: .touchUpInside)
     }
+    
     
 
 }
